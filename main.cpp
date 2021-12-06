fed03f3161bad9d06bfa8efc9130ef4a88786b36
A31Nesta <anthroponesta@gmail.com>
2021-12-06 22:06:33 +0100

    opengl libraries linked and included

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// OpenGL
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// std
+#include <iostream>
+
+int main() {
+	std::cout << "The libraries work I think" << std::endl;
+}
